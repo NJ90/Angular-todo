19179350cb5283ba76a6da45855eb69b8dba10d3
nameejang <55773845+nameejang@users.noreply.github.com>
2019-11-04 17:09:16 +0900

    todo list CR 완성

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -6,10 +6,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+  todos: Todo[];//todo-list 배열 생성해줌
+  todoTitle: string;
+  idForTodo: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.idForTodo = 4;
+    this.todoTitle = '';
+    this.todos = [
+      {
+        'id':1,
+        'title':'todo-1',
+        'completed': false,
+        'editing': false,
+      },
+      {
+        'id':2,
+        'title':'todo-2',
+        'completed': false,
+        'editing': false,
+      },
+      {
+        'id':3,
+        'title':'todo-3',
+        'completed': false,
+        'editing': false,
+      },
+    ];
   }
 
+  addTodo():void{
+    if (this.todoTitle.trim().length === 0){
+      return; //빈칸일때 입력 안되도록
+    }
+
+    this.todos.push({
+      id: this.idForTodo,//id값을 this에서 찾도록
+      title: this.todoTitle,
+      completed:false,
+      editing:false
+    })
+
+   this.todoTitle='';//입력후에 빈칸으로 
+   this.idForTodo++;//아이디 값이 ++되도록
+  }
+
+
+  //delet
+  deleteTodo(id: Number):void{
+    this.todos = this.todos.filter(todo => todo.id !== id);
+  }
+}
+
+
+interface Todo{
+  id: number,
+  title: string,
+  completed: boolean,
+  editing: boolean
 }
