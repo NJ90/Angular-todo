c8e59f2ceafac9ae0c5c4ff8fd87787735254487
nameejang <55773845+nameejang@users.noreply.github.com>
2019-11-06 16:45:41 +0900

    separate todo-item component done

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Todo } from 'src/app/interfaces/Todo';
+// import { EventEmitter } from 'events';
 
 @Component({
   selector: 'todo-item',
@@ -7,12 +8,34 @@ import { Todo } from 'src/app/interfaces/Todo';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
- @Input() todo: Todo; //todo-list(parent)로부터 input 을 rendering / todo-list.component.html 의 [todo]=""와 연결
+  @Input() todo: Todo; //todo-list(parent)로부터 input 을 rendering / todo-list.component.html 의 [todo]="todo"와 연결
+  @Output() checkedItem = new EventEmitter();
+  @Output() dblclickedItem = new EventEmitter();
+  // @Output() bluredItem = new EventEmitter();
+  // @Output() enteredItem = new EventEmitter();
+  @Output() canceledItem = new EventEmitter();
+  @Output() deletedItem = new EventEmitter();
   
  
  constructor() { }
 
   ngOnInit() {
+  }
+
+  doneEdit(todo : Todo) : void {
+    this.checkedItem.emit(todo);
+  }
+
+  editTodo(todo : Todo) : void{
+    this.dblclickedItem.emit(todo);
+  }
+
+  cancelEdit(todo : Todo) : void{
+    this.canceledItem.emit(todo);
+  }
+
+  deleteTodo(todo : Todo) : void{
+    this.deletedItem.emit(todo);
   }
 
 }
